e513682d6167e4159c9f7c36c93a55395d416114
xuzhinan <xuzhinansb@gmail.com>
2014-10-22 23:12:18 -0700

    Diff caused by vim tab config

--- leetcode/Single_Number.cpp
+++ leetcode/Single_Number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-  int singleNumber(int A[], int n) {
-    // IMPORTANT: Please reset any member data you declared, as
-    // the same Solution instance will be reused for each test case.
+	int singleNumber(int A[], int n) {
+		// IMPORTANT: Please reset any member data you declared, as
+		// the same Solution instance will be reused for each test case.
     int result = 0;
     for(int i = 0; i < n; i++) {
       result ^= A[i];
-    } 
-    
+    }
+
     return result;
   }
 };
